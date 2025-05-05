7eb8c7c33109659833c9bb10836b30b89ca784da
Indalu <endalukalbesa511@gmail.com>
2025-05-05 19:52:17 +0400

    Big change happenned

--- FarmDelivery/src/main/java/app/Main.java
+++ FarmDelivery/src/main/java/app/Main.java
@@ -1,7 +1,7 @@
 package app;
-
 public class Main {
     public static void main(String[] args){
         System.out.println("Yohannis");
+        System.out.println("Here is my first Java program");
     }
 }
