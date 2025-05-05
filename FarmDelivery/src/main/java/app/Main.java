824f5be14a05618a1688e093ece1d5a77eedcfe9
Yohannis <yohannisadmasu05@gmail.com>
2025-05-05 20:01:11 +0400

    conflict is here

--- FarmDelivery/src/main/java/app/Main.java
+++ FarmDelivery/src/main/java/app/Main.java
@@ -3,5 +3,15 @@ package app;
 public class Main {
     public static void main(String[] args){
         System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+        System.out.println("Yohannis Indalu");
+
     }
 }
