e8ba8f125016ff1c18a91b42832b8eac1b5552fc
Yohannis <yohannisadmasu05@gmail.com>
2025-05-05 22:17:34 +0400

    update on product

--- FarmDelivery/src/main/java/model/Product.java
+++ FarmDelivery/src/main/java/model/Product.java
@@ -1,14 +1,69 @@
 package model;
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Product {
-    private int productId;
+    private String productId;
     private String name;
     private String description;
     private int price;
     private int quantityAvailable;
-    private Date harvestDate;
+    private LocalDate harvestDate;
 
+    public Product(String productId, String name, String description, int price, int quantityAvailable, LocalDate harvestDate){
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantityAvailable = quantityAvailable;
+        this.harvestDate = harvestDate;
 
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+    public void setProductId(String productId){
+        this.productId = productId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(int quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public LocalDate getHarvestDate() {
+        return harvestDate;
+    }
+
+    public void setHarvestDate(LocalDate harvestDate) {
+        this.harvestDate = harvestDate;
+    }
 }
