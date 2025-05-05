951c629c9fcca4524ca765039a0bc903a92f4ee4
Indalu <endalukalbesa511@gmail.com>
2025-05-05 18:12:38 +0400

    Project set up

---
+++ FarmDelivery/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.farmdelivery {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.farmdelivery to javafx.fxml;
+    exports org.example.farmdelivery;
+}
