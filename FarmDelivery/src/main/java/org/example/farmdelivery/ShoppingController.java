f72612d0a26087e0b1f7dc92f9e9c03c42f9f387
Yohannis <yohannisadmasu05@gmail.com>
2025-05-06 14:59:31 +0400

    working on dashoboard

--- FarmDelivery/src/main/java/org/example/farmdelivery/ShoppingController.java
+++ FarmDelivery/src/main/java/org/example/farmdelivery/ShoppingController.java
@@ -3,27 +3,22 @@ package org.example.farmdelivery;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-<<<<<<< HEAD
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-=======
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
->>>>>>> 2d282f805c369c30b1656550d77a7757769f781e
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
-
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 
 public class ShoppingController {
     public FlowPane productList;
@@ -34,20 +29,21 @@ public class ShoppingController {
     public Button searchButton;
     public TextField searchBox;
 
-    private ArrayList<Product> products=new ArrayList<>();
-    Product product=new Product("Cherry Tomato",
+    private ArrayList<Product> products = new ArrayList<>();
+
+    Product product = new Product("Cherry Tomato",
             "https://i.pinimg.com/736x/26/6c/82/266c82c35ea3e95a62a0ab7a46b55212.jpg",
             20.00);
-    Product product1=new Product("Strawberry",
+    Product product1 = new Product("Strawberry",
             "https://i.pinimg.com/736x/26/6c/82/266c82c35ea3e95a62a0ab7a46b55212.jpg",
             20.00);
-    Product product2=new Product("Blueberry",
+    Product product2 = new Product("Blueberry",
             "https://i.pinimg.com/736x/26/6c/82/266c82c35ea3e95a62a0ab7a46b55212.jpg",
             20.00);
-    Product product3=new Product("Blackberry",
+    Product product3 = new Product("Blackberry",
             "https://i.pinimg.com/736x/26/6c/82/266c82c35ea3e95a62a0ab7a46b55212.jpg",
             20.00);
-    Product product4=new Product("Raspberry",
+    Product product4 = new Product("Raspberry",
             "https://i.pinimg.com/736x/26/6c/82/266c82c35ea3e95a62a0ab7a46b55212.jpg",
             20.00);
 
@@ -58,12 +54,11 @@ public class ShoppingController {
         products.add(product2);
         products.add(product3);
         products.add(product4);
-        categoryComboBox.getItems().addAll("All","Milk Products","Fruits","Vegetables","Cereal Products");
-        sortComboBox.getItems().addAll("Descending","Ascending");
-
+        categoryComboBox.getItems().addAll("All", "Milk Products", "Fruits", "Vegetables", "Cereal Products");
+        sortComboBox.getItems().addAll("Descending", "Ascending");
 
         for (Product product : products) {
-          try {
+            try {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/farmdelivery/product-card.fxml"));
                 HBox productCard = loader.load();
                 ProductCard controller = loader.getController();
@@ -74,23 +69,25 @@ public class ShoppingController {
             }
         }
     }
+
     @FXML
     private Label welcomeText;
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
     public void handleAddItem(ActionEvent actionEvent) {
+        // Add item logic here
     }
 
     public void handleViewCart(ActionEvent actionEvent) {
+        // View cart logic here
     }
 
-
-<<<<<<< HEAD
     @FXML
-    private void handleDashBoard(ActionEvent event) throws IOException, IOException {
+    private void handleDashBoard(ActionEvent event) throws IOException {
         BorderPane dashboardRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/org/example/farmdelivery/farmer-dashboard.fxml")));
         Scene dashboardScene = new Scene(dashboardRoot);
 
@@ -98,24 +95,28 @@ public class ShoppingController {
         dashboardScene.getStylesheets().add(dashboardCss);
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
         stage.setScene(dashboardScene);
         stage.setTitle("Dashboard");
         stage.show();
-=======
+    }
+
     public void handleHome(ActionEvent actionEvent) {
+        // Home button logic
     }
 
     public void handleProducts(ActionEvent actionEvent) {
+        // Products button logic
     }
 
     public void handleCart(ActionEvent actionEvent) {
+        // Cart button logic
     }
 
     public void handleProfile(ActionEvent actionEvent) {
+        // Profile button logic
     }
 
     public void handleLogout(ActionEvent actionEvent) {
->>>>>>> 2d282f805c369c30b1656550d77a7757769f781e
+        // Logout logic
     }
 }
